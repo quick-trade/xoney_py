8ee0a3253f1a047fbb5ce8c0afbf03a17d1e14f1
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2022-08-18 01:54:52 +0300

    metrics

--- xoney/analysis/metrics/defaults.py
+++ xoney/analysis/metrics/defaults.py
@@ -12,3 +12,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =============================================================================
+from abc import ABC, abstractmethod
+
+from xoney.generic.equity import Equity
+from xoney.analysis.regression import (RegressionModel,
+                                       ExponentialRegression,
+                                       LinearRegression)
+
+
+class Metric(ABC):
+    _value: float
+
+    @property
+    def value(self) -> float:
+        return self._value
+
+    @abstractmethod
+    def calculate(self, equity: Equity) -> None:
+        ...
+
+
+class YearProfit(Metric):
+    __model: ExponentialRegression
+
+    def __init__(self):
+        self.__model = ExponentialRegression()
+
+    def calculate(self, equity: Equity) -> None:
+        self.__model.fit(array=equity.as_array())
+
+        regression = self.__model.curve
+
+        profit_per_candle: float = regression[1] / regression[0]
+        candles_per_year: float = equity.timeframe.candles_in_year
+
+        profit_per_year: float = profit_per_candle ** candles_per_year
+
+        self._value = profit_per_year
