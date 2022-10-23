6df0f558999c9e60be119c9a43f870c4ad0ab187
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2022-10-23 19:39:57 +0300

    tests for `analysis/metrics/defaults.py`

--- tests/stats/metrics_test.py
+++ tests/stats/metrics_test.py
@@ -12,9 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =============================================================================
-from xoney.analysis.metrics import YearProfit
+import numpy as np
+
+from xoney.analysis.metrics import *
 from xoney.generic.equity import Equity
-from xoney.generic.timeframes import HOUR_1
+from xoney.generic.timeframes import *
 
 from xoney.math import is_equal
 
@@ -25,9 +27,17 @@ import pytest
 def equity_d1_exp():
     return Equity([1, 2, 4, 8])
 
+
 @pytest.fixture
 def equity_h1_exp():
     return Equity([1, 1.01, 1.01**2], timeframe=HOUR_1)
+
+
+@pytest.fixture
+def equity_1d_15_pct_dd():
+    return Equity([100, 110, 110*0.9, 100, 110*0.85, 150, 140],
+                  timeframe=DAY_1)
+
 
 class TestYearProfit:
     def test_d1(self, equity_d1_exp):
@@ -41,3 +51,34 @@ class TestYearProfit:
         metric.calculate(equity=equity_h1_exp)
 
         assert is_equal(metric.value, 1.01**(365*24))
+
+
+class TestMaxDrawDown:
+    def test_d1(self, equity_d1_exp):
+        value = evaluate_metric(MaxDrawDown(), equity=equity_d1_exp)
+        assert is_equal(value, 0.0)
+
+    def test_h1(self, equity_h1_exp):
+        value = evaluate_metric(MaxDrawDown, equity=equity_h1_exp)
+        assert is_equal(value, 0.0)
+
+    def test_d1_15_pct(self, equity_1d_15_pct_dd):
+        metric = MaxDrawDown()
+        metric.calculate(equity_1d_15_pct_dd)
+
+        assert is_equal(metric.value, 0.15)
+
+
+class TestCalmarRatio:
+    def test_d1(self, equity_d1_exp):
+        value = evaluate_metric(CalmarRatio, equity_d1_exp)
+        assert is_equal(value, np.inf)
+
+    def test_d1_15_pct(self, equity_1d_15_pct_dd):
+        value = evaluate_metric(CalmarRatio, equity_1d_15_pct_dd)
+        expected = evaluate_metric(
+            YearProfit,
+            equity_1d_15_pct_dd
+        ) / 0.15
+        assert is_equal(value,
+                        expected)
