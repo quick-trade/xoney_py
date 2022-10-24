837df4badac693678b71618f837e6cf77e48260e
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2022-10-24 14:57:03 +0300

    tests for Sharpe and Sortino ratios

--- tests/stats/metrics_test.py
+++ tests/stats/metrics_test.py
@@ -32,6 +32,10 @@ def equity_d1_exp():
 def equity_h1_exp():
     return Equity([1, 1.01, 1.01**2], timeframe=HOUR_1)
 
+@pytest.fixture
+def equity_d1_1_pct():
+    return Equity([1, 1.01, 1.01**2, 1.01**3], timeframe=DAY_1)
+
 
 @pytest.fixture
 def equity_1d_15_pct_dd():
@@ -82,3 +86,40 @@ class TestCalmarRatio:
         ) / 0.15
         assert is_equal(value,
                         expected)
+
+
+class TestSharpeRatio:
+    def test_1h(self, equity_h1_exp):
+        value = evaluate_metric(SharpeRatio, equity_h1_exp)
+        mean_profit = equity_h1_exp.change().mean()
+        std_profit = equity_h1_exp.change().std()
+        candles_sqrt = np.sqrt(equity_h1_exp.timeframe.candles_in_year)
+        expected = (mean_profit / std_profit) * candles_sqrt
+        assert is_equal(value, expected)
+
+    def test_d1_15_pct(self, equity_1d_15_pct_dd):
+        value = evaluate_metric(SharpeRatio, equity_1d_15_pct_dd)
+        mean_profit = equity_1d_15_pct_dd.change().mean()
+        std_profit = equity_1d_15_pct_dd.change().std()
+        candles_sqrt = np.sqrt(equity_1d_15_pct_dd.timeframe.candles_in_year)
+        expected = (mean_profit / std_profit) * candles_sqrt
+        assert is_equal(value, expected)
+
+
+class TestSortinoRatio:
+    @pytest.mark.parametrize("risk_free",
+                             [1,
+                              0.05,
+                              0.2])
+    def test_d1_15_pct(self, equity_1d_15_pct_dd, risk_free):
+        value = evaluate_metric(SortinoRatio(risk_free=risk_free),
+                                equity_1d_15_pct_dd)
+        candles = equity_1d_15_pct_dd.timeframe.candles_in_year
+        returns = equity_1d_15_pct_dd.change().as_array()
+        mean_profit = returns.mean()
+        std_profit = returns[returns < 0].std()
+
+        expected = (mean_profit * candles - risk_free) / \
+                   (std_profit * candles**0.5)
+        assert is_equal(value, expected)
+
