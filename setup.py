09f40796b55023588cb31b2ed9689a0649385576
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2022-11-11 01:56:24 +0200

    deps

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Vladyslav Kochetov. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
 from setuptools import find_packages
 from distutils.core import setup
 
@@ -5,7 +19,7 @@ from distutils.core import setup
 with open('README.md') as file:
     long_desc = file.read()
 
-__version__ = "0.0.8dev"
+__version__ = "0.0.9dev"
 
 packages = find_packages()
 for package in packages:
@@ -30,7 +44,7 @@ setup(
         'Bug Tracker': f"{repository_url}/issues"
     },
     install_requires=[
-        "numpy==1.23.4",
+        "pandas==1.5.1",
         "optuna==2.10.0",
         "scipy==1.9.3"
     ],
