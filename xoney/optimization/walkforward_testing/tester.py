52cd1781868ab6d9bb57772cfc8606e33fa126d0
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2023-02-10 10:54:40 +0200

    optimization of trading systems

--- xoney/optimization/walkforward_testing/tester.py
+++ xoney/optimization/walkforward_testing/tester.py
@@ -12,5 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =============================================================================
-from xoney.optimization.optimizer import Optimizer
-from xoney.optimization.optimizers import DefaultOptimizer
+from xoney.generic.workers import Worker
+
+
+class WalkForward(Worker):
+    ... # TODO
